ffdaf593612ee4d9452095706ac8aeb24a58cfaf
Tyler Hayes <tylerxhayes@gmail.com>
2019-02-13 11:45:20 -0800

    refactor gallery.js

--- scripts/src/gallery.js
+++ scripts/src/gallery.js
@@ -2,15 +2,25 @@ let images = [];
 let imgPointer = 0;
 let canSetImgPointer = true;
 
+let rightArrowKey = 39;
+let downArrowKey = 40;
+let leftArrowKey = 37;
+let upArrowKey = 38;
+
+let leftBar;
+let rightBar;
+
 $(document).ready(() => {
   images = $(".galleryBox");
+  leftBar = $("#leftBar");
+  rightBar = $("#rightBar");
   setImgPointer();
 }).keydown((event) => {
-  if (event.which == 38 || event.which == 37) {
+  if (event.which == upArrowKey || event.which == leftArrowKey) {
     prevPhoto();
     event.preventDefault();
   }
-  if (event.which == 40 || event.which == 39) {
+  if (event.which == downArrowKey || event.which == rightArrowKey) {
     nextPhoto();
     event.preventDefault();
   }
@@ -21,11 +31,12 @@ $(document).ready(() => {
 )
 
 function setImgPointer() {
+  // Determine last photo viewed based on proximity to top of viewport
   for (let i = 0; i < images.length; i++) {
     let img = images[i];
     let imgTopDistance = img
-    .getBoundingClientRect()
-    .top;
+      .getBoundingClientRect()
+      .top;
     if (imgTopDistance < 100 && imgTopDistance > -800) {
       imgPointer = i;
     }
@@ -34,32 +45,28 @@ function setImgPointer() {
 }
 
 function isFirstOrLast() {
-  let leftBar = $("#leftBar");
-  let rightBar = $("#rightBar");
   if (imgPointer == 0) {
-    if (!leftBar.hasClass("hide"))
+    if (!leftBar.hasClass("hide")) 
       leftBar.addClass("hide");
-  }
+    }
   else if (imgPointer == images.length - 1) {
-    if (!rightBar.hasClass("hide"))
+    if (!rightBar.hasClass("hide")) 
       rightBar.addClass("hide");
-  }
+    }
   else {
-    if (leftBar.hasClass("hide"))
+    if (leftBar.hasClass("hide")) 
       leftBar.removeClass("hide");
-    if (rightBar.hasClass("hide"))
+    if (rightBar.hasClass("hide")) 
       rightBar.removeClass("hide");
+    }
   }
-}
 
 function nextPhoto() {
-  canSetImgPointer = false;
-  setTimeout(() => {
-    canSetImgPointer = true;
-  }, 100);
+  blockImgPointerUpdate();
   if (imgPointer < images.length - 1) {
-    imgPointer++;
-    if ($(images[imgPointer]).hasClass("half")) 
+    if ($(images[imgPointer]).hasClass("half") && imgPointer < images.length - 2) 
+      imgPointer += 2;
+    else 
       imgPointer++;
     images[imgPointer].scrollIntoView();
   }
@@ -67,15 +74,20 @@ function nextPhoto() {
 }
 
 function prevPhoto() {
-  canSetImgPointer = false;
-  setTimeout(() => {
-    canSetImgPointer = true;
-  }, 100);
+  blockImgPointerUpdate();
   if (imgPointer > 0) {
+    if ($(images[imgPointer]).hasClass("half") && imgPointer > 1) 
+    imgPointer -= 2;
+    else 
     imgPointer--;
-    if ($(images[imgPointer]).hasClass("half")) 
-      imgPointer--;
     images[imgPointer].scrollIntoView();
   }
   isFirstOrLast();
 }
+
+function blockImgPointerUpdate() {
+  canSetImgPointer = false;
+  setTimeout(() => {
+    canSetImgPointer = true;
+  }, 100);
+};
